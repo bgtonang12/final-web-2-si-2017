5a1f9c91dd27854040839f58fd35d60df6c3e8d1
bgtonang12 <christianarios185@gmail.com>
2017-05-10 22:48:55 +0700

    update

--- Wiki viewer/js/index.js
+++ Wiki viewer/js/index.js
@@ -11,8 +11,6 @@ $("#display").empty();
 	$.getJSON("https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch="+sq+"&srnamespace=0&srwhat=text&titles=Main%20Page&callback=?", function(d) {
 
 		$.each(d.query.search, function(i, item){
-			//console.log(item.title);
-			//console.log(item.snippet);
 var wiki = '<div class="entry">'+
 	'<div class="title">'+item.title+'</div>'+
 	'<div class="snippet">'+item.snippet+'</div></div>';
