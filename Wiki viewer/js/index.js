eb7f3c988c30f7f005c878a09861d63d13a053e8
bgtonang12 <christianarios185@gmail.com>
2017-05-10 22:40:24 +0700

    update

---
+++ Wiki viewer/js/index.js
@@ -0,0 +1,23 @@
+//code ini di ambil dari youtube
+$('#search-form').submit(function(e){
+	e.preventDefault();
+});
+
+function search(){
+$("#display").empty();
+
+	sq = $('#srch').val();
+
+	$.getJSON("https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch="+sq+"&srnamespace=0&srwhat=text&titles=Main%20Page&callback=?", function(d) {
+
+		$.each(d.query.search, function(i, item){
+			//console.log(item.title);
+			//console.log(item.snippet);
+var wiki = '<div class="entry">'+
+	'<div class="title">'+item.title+'</div>'+
+	'<div class="snippet">'+item.snippet+'</div></div>';
+			$("#display").append(wiki);
+		});
+	});
+$('#srch').val('');
+}
